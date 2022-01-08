b75cfa3d7b0be33254812a4541fafed264d7c0d0
Jhonatan <yamidjhonatan@gmail.com>
2022-01-07 21:25:37 -0500

    Se valida autoloop

--- src/components/ItemCategory.js
+++ src/components/ItemCategory.js
@@ -1,8 +1,20 @@
 import Link from 'next/link';
 import React from 'react'
 import Lottie from "react-lottie";
+import { useState } from 'react/cjs/react.development';
 
 const ItemCategory = ({ title, image }) => {
+    const [autoloop, setAutoloop] = useState(false)
+    React.useEffect(() => {
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            setAutoloop(true)
+        } 
+    }, [])
+
+
+
+
+
     var path = `/images/${image}.png`
     React.useEffect(() => {
         import("lottie-interactive/dist/lottie-interactive.js");
@@ -12,9 +24,11 @@ const ItemCategory = ({ title, image }) => {
             <a className='w-1/3 md:w-1/5 rounded-3xl bg-white m-2 shadow-lg py-5 flex justify-center'>
                 <div className='w-24 text-xl hover:text-2xl'>
                     <lottie-interactive
+                        autoplay={autoloop}
+                        loop={autoloop}
                         path={image}
                         interaction="morph-lock"
-                        speed="3"
+                        speed="1.5"
                     />
                     <h2 className='font-semibold text-teal-600 text-center drop-shadow-lg'>{title}</h2>
                 </div>
