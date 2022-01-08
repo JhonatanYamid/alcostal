0abad44c71abc12a9fb6d354ccced994496922b7
Jhonatan <yamidjhonatan@gmail.com>
2022-01-07 20:55:41 -0500

    Ajuste categorias

--- src/components/ItemCategory.js
+++ src/components/ItemCategory.js
@@ -1,18 +1,22 @@
 import Link from 'next/link';
 import React from 'react'
+import Lottie from "react-lottie";
 
 const ItemCategory = ({ title, image }) => {
     var path = `/images/${image}.png`
+    React.useEffect(() => {
+        import("lottie-interactive/dist/lottie-interactive.js");
+    }, []);
     return (
         <Link href="/">
             <a className='w-1/3 md:w-1/5 rounded-3xl bg-white m-2 shadow-lg py-5 flex justify-center'>
-                <div className='drop-shadow-xl w-20 hover:w-24 text-xl hover:text-2xl transition'>
-                    <img
-                        src={path}
-                        alt="category item"
-                        className=''
+                <div className='w-24 text-xl hover:text-2xl'>
+                    <lottie-interactive
+                        path={image}
+                        interaction="morph-lock"
+                        speed="3"
                     />
-                    <h2 className='font-semibold text-teal-600 text-center drop-shadow-lg mt-3'>{title}</h2>
+                    <h2 className='font-semibold text-teal-600 text-center drop-shadow-lg'>{title}</h2>
                 </div>
 
             </a>
--- src/components/Section3.js
+++ src/components/Section3.js
@@ -4,7 +4,7 @@ import ItemCategory from './ItemCategory';
 const Section3 = () => {
     return (
         <div>
-            <svg  viewBox="0 0 1440 320">
+            <svg viewBox="0 0 1440 320">
                 <path fill="#14b8a6" d="M0,32L60,58.7C120,85,240,139,360,149.3C480,160,600,128,720,112C840,96,960,96,1080,106.7C1200,117,1320,139,1380,149.3L1440,160L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
             </svg>
             <div className='w-full flex flex-col bg-teal-500 md:-mt-24'>
@@ -13,22 +13,23 @@ const Section3 = () => {
                     <h3 className='text-3xl font-bold text-white'>Dando click sobre una opción</h3>
                 </div>
                 <div className='md:px-10 flex flex-row pt-5 flex-wrap justify-center pb-8 md:pb-0'>
+
                     <ItemCategory
                         title="Verduras"
-                        image="asparagus" />
+                        image="/lottieFiles/63272-walking-avocado.json" />
                     <ItemCategory
-                        title="Verduras"
-                        image="fruits" />
+                        title="Frutas"
+                        image="/lottieFiles/62266-walking-orange.json" />
                     <ItemCategory
-                        title="Verduras"
-                        image="salad" />
+                        title="Plantas"
+                        image="/lottieFiles/62761-walking-pothos.json" />
                     <ItemCategory
-                        title="Verduras"
-                        image="tofu" />
+                        title="Animales"
+                        image="/lottieFiles/56305-cool-turkey.json" />
                 </div>
             </div>
             <div>
-                <svg  viewBox="0 0 1440 320">
+                <svg viewBox="0 0 1440 320">
                     <path fill="#14b8a6" d="M0,32L60,58.7C120,85,240,139,360,149.3C480,160,600,128,720,112C840,96,960,96,1080,106.7C1200,117,1320,139,1380,149.3L1440,160L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path>
                 </svg>
             </div>
