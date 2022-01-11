b8b93573ecfdfa2c5910a2fb87dabe096e4b8695
Jhonatan <yamidjhonatan@gmail.com>
2022-01-11 00:17:03 -0500

    Se agrega lottie

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -3,6 +3,11 @@ import React from 'react'
 import SearchInput from './SearchInput';
 
 const Jumbotron = () => {
+
+    React.useEffect(() => {
+        import("lottie-interactive/dist/lottie-interactive.js");
+    }, []);
+
     return (
         <div
             className="w-full h-screen bg-no-repeat bg-left-bottom bg-auto md:bg-contain bg-[url('/images/bg-jumbotron.png')] flex flex-wrap-reverse justify-around items-center"
@@ -12,12 +17,17 @@ const Jumbotron = () => {
                 <p className='text-xl'>Empecemos buscando un producto fresco y recién sacadito de la tierra, aquí abajo</p>
                 <SearchInput />
             </div>
-            <div className='md:w-1/2 md:h-1/2 px-5 md:px-0 pt-20 md:pt-0'>
-                <img
+            <div className='md:w-1/2 md:h-1/2 px-5 md:px-0 md:-ml-32 pt-20 md:pt-0'>
+                {/* <img
                     src="/images/character-jumbotron.png"
                     alt="character-jumbotron"
                     width={500}
                     height={300}
+                /> */}
+                <lottie-interactive
+                    autoplay
+                    loop
+                    path="/lottieFiles/jumbotron.json"
                 />
             </div>
 
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,12 +3,12 @@ import React from 'react'
 const Footer = () => {
     return (
         
-        <div className="-mt-24 md:-mt-52 backgroundSection md:bg-cover justify-center bg-[url('/images/bg-footer.png')]">
+        <div className="-mt-24 md:-mt-48 backgroundSection md:bg-cover justify-center bg-[url('/images/bg-footer.png')]">
             <div className='w-3/4 md:w-1/2 rounded-xl bg-white flex justify-center px-5 md:px-20 py-10 flex-col text-center'>
                 <h2 className='font-bold text-2xl text-stone-800'>SIGANOS EN FEISBÚ</h2>
-                <h3 className='text-lg pt-5'>Contactenos por cualquiera de las redes sociales de aquí abajo y siganos también...<br />¡Para que no se pierda ni media!</h3>
+                <h3 className='text-md pt-5'>Contactenos por cualquiera de las redes sociales de aquí abajo y siganos también...<br />¡Para que no se pierda ni media!</h3>
                 {/* Iconos */}
-                <div className='flex flex-row pt-7 justify-center'>
+                <div className='flex flex-row pt-5 justify-center'>
                     <svg className='w-10 mr-2' viewBox="0 0 25 25" role="img" fill="#444" ><path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" /></svg>
                     <svg className='w-16' viewBox="0 0 27 27" role="img" fill="#444" ><path d="M2.004 22l1.352-4.968A9.954 9.954 0 0 1 2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10a9.954 9.954 0 0 1-5.03-1.355L2.004 22zM8.391 7.308a.961.961 0 0 0-.371.1 1.293 1.293 0 0 0-.294.228c-.12.113-.188.211-.261.306A2.729 2.729 0 0 0 6.9 9.62c.002.49.13.967.33 1.413.409.902 1.082 1.857 1.971 2.742.214.213.423.427.648.626a9.448 9.448 0 0 0 3.84 2.046l.569.087c.185.01.37-.004.556-.013a1.99 1.99 0 0 0 .833-.231c.166-.088.244-.132.383-.22 0 0 .043-.028.125-.09.135-.1.218-.171.33-.288.083-.086.155-.187.21-.302.078-.163.156-.474.188-.733.024-.198.017-.306.014-.373-.004-.107-.093-.218-.19-.265l-.582-.261s-.87-.379-1.401-.621a.498.498 0 0 0-.177-.041.482.482 0 0 0-.378.127v-.002c-.005 0-.072.057-.795.933a.35.35 0 0 1-.368.13 1.416 1.416 0 0 1-.191-.066c-.124-.052-.167-.072-.252-.109l-.005-.002a6.01 6.01 0 0 1-1.57-1c-.126-.11-.243-.23-.363-.346a6.296 6.296 0 0 1-1.02-1.268l-.059-.095a.923.923 0 0 1-.102-.205c-.038-.147.061-.265.061-.265s.243-.266.356-.41a4.38 4.38 0 0 0 .263-.373c.118-.19.155-.385.093-.536-.28-.684-.57-1.365-.868-2.041-.059-.134-.234-.23-.393-.249-.054-.006-.108-.012-.162-.016a3.385 3.385 0 0 0-.403.004z" /></svg>
 
