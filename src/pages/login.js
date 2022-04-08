778eb31b930e0e685879c680da187396e373a163
Jhonatan Yamid Alzate <53678639+JhonatanYamid@users.noreply.github.com>
2022-04-08 01:35:22 -0500

    Update login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,5 @@
 import Layout from "../components/layout/Layout";
-// import LoginForm from "../components/LoginForm";
+import LoginForm from "../components/LoginForm";
 
 const Login = () => {
     return (
@@ -9,7 +9,7 @@ const Login = () => {
                     <img src="images/logo_blanco.png" className="w-3/5" alt="logo_blanco" />
                 </div>
                 <div className="flex flex-col bg-white rounded-lg mt-8 md:mt-0 justify-center items-center w-full md:w-[80%] h-[60%] md:h-full space-y-3 text-gray-700">
-                    {/* <LoginForm /> */}
+                    <LoginForm />
                 </div>
             </div>
         </Layout>
