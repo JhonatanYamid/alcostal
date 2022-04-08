43965407dc3f971f338b5c559a1aadb4faaf002b
Jhonatan <yamidjhonatan@gmail.com>
2022-04-08 01:50:32 -0500

    Fix: Fixed menu

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React, { useState } from 'react'
-// import { responsiveNavbar, changeNavbackground } from '../helper';
+import { responsiveNavbar} from '../helper';
 
 const Navbar = () => {
 
